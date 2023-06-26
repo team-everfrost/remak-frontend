a4cde11ca37869c093bc9c5d1b2f95376ebd1769
qilip <qilip@qilip.io>
2023-06-26 18:29:36 +0900

    🍎 Chore: vue lint 설정 추가
    #

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,11 +8,16 @@ module.exports = {
   parserOptions: {
     parser: '@typescript-eslint/parser',
   },
-  extends: ['@nuxtjs/eslint-config-typescript', 'plugin:prettier/recommended'],
+  extends: [
+    '@nuxtjs/eslint-config-typescript',
+    'plugin:vue/vue3-recommended',
+    'plugin:prettier/recommended',
+  ],
   plugins: [],
   rules: {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'vue/multi-word-component-names': 'off',
   },
 };
