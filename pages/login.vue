8f1b6b010c015c678e39df4e3fd25b97d243d7eb
qilip <qilip@qilip.io>
2023-07-01 15:06:48 +0900

    🌈 Style: class 순서
    #

--- pages/login.vue
+++ pages/login.vue
@@ -6,9 +6,9 @@ useSeoMeta({
 
 <template>
   <div
-    class="min-h-screen flex flex-col items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8"
+    class="flex min-h-screen flex-col items-center justify-center bg-gray-50 px-4 py-12 sm:px-6 lg:px-8"
   >
-    <div class="max-w-sm w-full space-y-8">
+    <div class="w-full max-w-sm space-y-8">
       <div class="text-center">
         <NuxtLink to="/">Remak</NuxtLink>
       </div>
@@ -19,9 +19,9 @@ useSeoMeta({
       </div>
       <form class="mt-8 space-y-6">
         <input type="hidden" name="remember" value="true" />
-        <div class="rounded-md shadow-sm -space-y-px">
+        <div class="-space-y-px rounded-md shadow-sm">
           <div>
-            <label for="email-address" class="text-gray-600 text-sm"
+            <label for="email-address" class="text-sm text-gray-600"
               >이메일</label
             >
             <input
@@ -30,19 +30,19 @@ useSeoMeta({
               type="email"
               autocomplete="email"
               required
-              class="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-gray-500 focus:border-gray-500 focus:z-10"
+              class="relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-gray-500 focus:outline-none focus:ring-gray-500"
               placeholder="이메일 주소를 입력하세요"
             />
           </div>
           <div class="pt-4">
-            <label for="password" class="text-gray-600 text-sm">비밀번호</label>
+            <label for="password" class="text-sm text-gray-600">비밀번호</label>
             <input
               id="password"
               name="password"
               type="password"
               autocomplete="current-password"
               required
-              class="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-gray-500 focus:border-gray-500 focus:z-10"
+              class="relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-gray-500 focus:outline-none focus:ring-gray-500"
               placeholder="비밀번호를 입력하세요"
             />
           </div>
@@ -50,7 +50,7 @@ useSeoMeta({
         <div>
           <button
             type="submit"
-            class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-gray-600 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+            class="group relative flex w-full justify-center rounded-md border border-transparent bg-gray-600 px-4 py-2 text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2"
           >
             로그인
           </button>
