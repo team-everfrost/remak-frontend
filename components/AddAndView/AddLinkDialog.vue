ce04ebe10812dedcd55e0145bd0e03aec3d0c3c1
song-yeonghyun <dudgus907@naver.com>
2023-10-09 19:29:49 +0900

    🎨 Design: 링크 추가 다이얼로그 디자인
    #

---
+++ components/AddAndView/AddLinkDialog.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="relative flex w-[480px] flex-col rounded-[20px] bg-white">
+    <div class="flex flex-row items-center justify-between pl-5 pr-3 pt-5">
+      <p class="leading-18 text-lg font-bold text-[#1b1c1f]">추가하기</p>
+      <button>
+        <svg
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          class="relative h-6 w-6"
+          preserveAspectRatio="xMidYMid meet"
+        >
+          <path
+            d="M18 6L6 18M6 6L18 18"
+            stroke="#28323C"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          ></path>
+        </svg>
+      </button>
+    </div>
+    <textarea
+      placeholder="예시: www.naver.com, www.google.com..."
+      class="relative ml-4 mr-4 mt-8 h-[278px] flex-grow resize-none self-stretch overflow-auto rounded-xl border border-[#e6e8eb] bg-[#fefefe] p-4 outline-none"
+      rows="1"
+    ></textarea>
+    <p class="relative ml-4 mr-4 mt-3 text-left text-sm text-[#646f7c]">
+      엔터 또는 ,로 구분하여 여러 링크를 추가하세요
+    </p>
+    <button
+      class="relative mb-5 ml-5 mr-5 mt-6 flex h-[52px] flex-shrink-0 flex-grow-0 items-center justify-center self-stretch overflow-hidden rounded-xl bg-[#1f8ce6]"
+    >
+      <p class="flex-grow text-center text-lg font-bold text-white">추가하기</p>
+    </button>
+  </div>
+</template>
