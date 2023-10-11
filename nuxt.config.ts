24ba763d57963d6e6b6cd3e824840a95cda65754
qilip <qilip@qilip.io>
2023-10-12 06:32:51 +0900

    ✨ Feat: 로그인 기능 구현
    #

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,17 +2,25 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt'],
+  pinia: {
+    autoImports: ['defineStore', 'acceptHMRUpdate'],
+  },
   app: {
     head: {
       title: 'Remak',
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { name: 'msapplication-TileColor', content: '#ffffff' },
-        { name: 'msapplication-TileImage', content: '/ms-icon-144x144.png' },
-        { name: 'theme-color', content: '#ffffff' },
+        // { name: 'msapplication-TileColor', content: '#1F8CE6' },
+        // { name: 'msapplication-TileImage', content: '/ms-icon-144x144.png' },
+        { name: 'theme-color', content: '#1F8CE6' },
       ],
       link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    },
+  },
+  runtimeConfig: {
+    public: {
+      apiBaseUrl: process.env.API_BASE_URL ?? 'https://api-dev.remak.io',
     },
   },
 });
--- pages/login.vue
+++ pages/login.vue
@@ -1,34 +1,55 @@
-<script setup>
-useSeoMeta({
-  title: '로그인 - Remak',
-});
-</script>
-
 <template>
   <div>
     <TopBarBase />
     <div class="flex min-h-screen items-center justify-center bg-white">
       <div class="text-center">
         <div class="mb-4 text-2xl font-bold">
-          <span class="text-sky-500">쉽다, 빠르다, 편리하다</span>
-          <span class="text-gray-800">REMAK</span>
+          <span class="text-sky-500">쉽다, 빠르다, 편리하다 </span>
+          <span class="text-gray-800">Remak</span>
         </div>
         <input
           v-model="email"
           type="email"
           placeholder="이메일 주소를 입력하세요"
-          class="mb-4 mt-[48px] block h-[56px] w-[440px] rounded-xl border border-gray-300 bg-white px-4 py-2"
+          class="mt-[48px] block h-[56px] w-[440px] rounded-xl border bg-white px-4 py-2"
+          :class="{
+            'border-gray-300 focus:outline-remak-blue': !emailNotExists,
+            'border-red-500 focus:outline-red-500': emailNotExists,
+          }"
+          autofocus
           @input="checkEmail"
+          @keyup.enter="handleNextClick"
         />
+        <div class="h-4">
+          <span v-if="emailNotExists" class="text-xs text-red-500"
+            >존재하지 않는 이메일입니다.</span
+          >
+        </div>
         <input
-          v-if="showPasswordInput"
+          v-show="showPasswordInput"
+          ref="passwordInput"
+          v-model="password"
           type="password"
           placeholder="비밀번호를 입력하세요"
-          class="mb-4 mt-4 block h-[56px] w-[440px] rounded-lg border border-gray-300 px-4 py-2"
+          class="mb-4 mt-4 block h-[56px] w-[440px] rounded-lg border px-4 py-2"
+          :class="{
+            'border-gray-300 focus:outline-remak-blue': !passwordWrong,
+            'border-red-500 focus:outline-red-500': passwordWrong,
+          }"
+          @input="() => (passwordWrong = false)"
+          @keyup.enter="handleNextClick"
         />
+        <div class="h-4">
+          <span v-if="passwordWrong" class="text-xs text-red-500"
+            >잘못된 비밀번호입니다.</span
+          >
+        </div>
         <button
-          class="mb-4 mt-[48px] block h-[56px] w-[440px] rounded-lg border bg-zinc-100 px-4 py-2 text-neutral-300"
-          :class="emailClass"
+          class="mb-4 mt-[32px] block h-[56px] w-[440px] rounded-lg border px-4 py-2"
+          :class="{
+            'bg-remak-blue text-white': isValidEmail(email),
+            'bg-gray-300 text-gray-500': !isValidEmail(email),
+          }"
           :disabled="!isValidEmail(email)"
           @click="handleNextClick"
         >
@@ -42,33 +63,59 @@ useSeoMeta({
   </div>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      email: '',
-      showPasswordInput: false,
-    };
-  },
-  computed: {
-    emailClass() {
-      if (this.isValidEmail(this.email)) {
-        return 'bg-remak-blue text-white';
-      }
-      return 'bg-gray-300 text-gray-500';
-    },
-  },
-  methods: {
-    checkEmail() {
-      this.isValidEmail(this.email);
-    },
-    isValidEmail(email) {
-      const regex = /^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
-      return regex.test(email);
-    },
-    handleNextClick() {
-      this.showPasswordInput = true;
-    },
-  },
+<script setup lang="ts">
+import { useAuthStore } from '~/stores/auth';
+
+useSeoMeta({
+  title: '로그인 - Remak',
+});
+
+const email = ref('');
+const password = ref('');
+const showPasswordInput = ref(false);
+const emailNotExists = ref(false);
+const passwordWrong = ref(false);
+
+const passwordInput = ref<HTMLInputElement | null>(null);
+
+const authStore = useAuthStore();
+
+const checkEmail = () => {
+  isValidEmail(email.value);
+  showPasswordInput.value = false;
+  emailNotExists.value = false;
+};
+
+const isValidEmail = (email: string) => {
+  const regex = /^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+  return regex.test(email);
+};
+
+const handleNextClick = async () => {
+  if (!isValidEmail(email.value)) {
+    return;
+  }
+
+  if (showPasswordInput.value) {
+    if (await authStore.signIn(email.value, password.value)) {
+      navigateTo('/');
+    } else {
+      passwordWrong.value = true;
+      nextTick(() => {
+        passwordInput.value?.focus();
+      });
+    }
+    return;
+  }
+
+  if (await authStore.checkEmail(email.value)) {
+    showPasswordInput.value = true;
+    emailNotExists.value = false;
+    nextTick(() => {
+      passwordInput.value?.focus();
+    });
+  } else {
+    emailNotExists.value = true;
+  }
 };
 </script>
