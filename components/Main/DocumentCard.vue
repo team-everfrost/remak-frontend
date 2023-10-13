a4377ed27ef15a6a5903e6fe72dc53715ecdf3d4
qilip <qilip@qilip.io>
2023-10-13 14:01:09 +0900

    ✨ Feat: 메인화면 작업중
    #

--- components/Main/DocumentCard.vue
+++ components/Main/DocumentCard.vue
@@ -1,40 +1,91 @@
 <template>
   <div
-    class="flex w-[258px] flex-shrink-0 flex-grow-0 flex-col items-start justify-start gap-5 overflow-hidden rounded-2xl border border-[#e6e8eb] bg-[#fefefe] px-3.5 pb-4 pt-5"
+    class="flex w-full flex-shrink-0 flex-grow-0 flex-col items-start justify-start overflow-hidden rounded-2xl border border-[#e6e8eb] bg-[#fefefe] pb-4"
+    :class="!imageUrl ? 'gap-0 pt-4' : 'gap-4 pt-0'"
     style="
-      box-shadow: 0px 0px 1px 0 rgba(65, 74, 82, 0.04),
+      box-shadow:
+        0px 0px 1px 0 rgba(65, 74, 82, 0.04),
         0px 2px 4px 0 rgba(102, 110, 119, 0.16);
     "
   >
+    <div class="w-full bg-[#F4F6F8]">
+      <svg
+        v-if="imageUrl"
+        width="258"
+        height="160"
+        viewBox="0 0 258 160"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-40 flex-shrink-0 flex-grow-0 mx-auto"
+        preserveAspectRatio="none"
+      >
+        <rect width="258" height="160" fill="#F4F6F8"></rect>
+        <path
+          d="M104.7 75.0778V56.4539C104.7 55.2919 105.639 54.35 106.801 54.35H134.674C145.776 54.35 153.3 64.1811 153.3 73.39C153.3 80.7571 148.869 84.4725 145.498 88.3303L151.871 101.743C152.535 103.139 151.517 104.75 149.971 104.75H133.638C133.354 104.75 133.082 104.635 132.884 104.432L104.998 75.8119C104.807 75.6155 104.7 75.3521 104.7 75.0778Z"
+          fill="#E0E0E0"
+        ></path>
+        <path
+          d="M104.7 102.646V84.4198C104.7 83.9512 105.267 83.7166 105.598 84.0479L125.402 103.852C125.733 104.183 125.499 104.75 125.03 104.75H106.804C105.642 104.75 104.7 103.808 104.7 102.646Z"
+          fill="#E0E0E0"
+        ></path>
+      </svg>
+    </div>
     <div
-      class="flex flex-shrink-0 flex-grow-0 flex-col items-start justify-start gap-5"
+      class="flex flex-shrink-0 flex-grow-1 flex-col items-start justify-start gap-5 px-3.5"
     >
       <div
-        class="relative flex flex-shrink-0 flex-grow-0 flex-col items-start justify-start gap-3"
+        class="flex flex-shrink-0 flex-grow-1 flex-col items-start justify-start gap-3"
       >
         <p
-          class="line-clamp-2 w-[228px] flex-shrink-0 flex-grow-0 text-left text-base font-bold text-[#111]"
+          class="line-clamp-2 flex-shrink-1 flex-grow-1 text-left text-base font-bold text-[#111]"
         >
-          “iOS 앱도 구글 툴로”··· 구글, 다트 3와 플러터 3.10 출시
+          {{ title }}
         </p>
         <p
-          class="line-clamp-3 max-h-[4.5em] flex-shrink-0 flex-grow-0 overflow-hidden text-left text-sm text-[#646f7c]"
+          class="line-clamp-3 flex-shrink-0 flex-grow-1 text-left text-sm text-[#646f7c]"
         >
-          누구든지 체포 또는 는 즉시 변호인의 조력을 받을 권리를누구든지 체포
-          또는 는 즉시 변호변호받을 권리를누구든지 체포 또는 는 즉시 변호변호
+          <span
+            class="flex-shrink-0 flex-grow-1 text-left text-sm text-[#646f7c]"
+          >
+            {{ summary }}
+          </span>
         </p>
       </div>
-      <div
-        class="relative flex flex-shrink-0 flex-grow-0 items-end justify-start gap-2"
-      >
-        <p class="flex-shrink-0 flex-grow-0 text-left text-xs text-[#646f7c]">
-          종류
+      <div class="flex flex-shrink-0 flex-grow-1 items-end justify-start gap-2">
+        <p class="flex-shrink-0 flex-grow-1 text-left text-xs text-[#646f7c]">
+          {{ typeKorean }}
         </p>
-        <div class="h-3 w-px flex-shrink-0 flex-grow-0 bg-[#646f7c]"></div>
-        <p class="flex-shrink-0 flex-grow-0 text-left text-xs text-[#646f7c]">
-          YYYY.MM.DD
+        <div class="h-3 w-px flex-shrink-0 flex-grow-1 bg-[#646f7c]"></div>
+        <p class="flex-shrink-0 flex-grow-1 text-left text-xs text-[#646f7c]">
+          {{ date }}
         </p>
       </div>
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  type: string;
+  imageUrl: string;
+  url: string;
+  title: string;
+  summary: string;
+  date: string;
+}>();
+
+const typeKorean = computed(() => {
+  switch (props.type) {
+    case 'MEMO':
+      return '메모';
+    case 'IMAGE':
+      return '이미지';
+    case 'WEBPAGE':
+      return '링크';
+    case 'FILE':
+      return '파일';
+    default:
+      return '메모';
+  }
+});
+</script>
--- pages/app/index.vue
+++ pages/app/index.vue
@@ -2,14 +2,10 @@
   <div class="flex h-screen flex-col">
     <TopBarBase />
     <div class="flex flex-grow flex-row">
-      <!-- mt-16을 추가하여 TopBarBase 아래에 공간을 제공 -->
       <SideNavigation class="mt-20" />
-      <!-- 나머지 콘텐츠는 이 공간에 배치 -->
-      <div class="mt-20 flex-grow bg-[#F4F6F8] pl-48">
-        <!-- pl-12를 추가하여 왼쪽 패딩 제공 -->
-        <!-- 여기에 콘텐츠 추가 -->
-        <div class="ml-20 mr-20 mt-20">
-          <div class="flex flex-row items-center justify-between">
+      <div class="mt-20 flex-grow bg-[#F4F6F8] ml-48">
+        <div class="flex flex-col m-20">
+          <div class="flex items-center justify-between mb-4">
             <p class="text-2xl font-bold text-[#1b1c1f]">메인</p>
             <button
               class="h-8 w-[78px] rounded-md bg-gray-600 text-base font-medium text-white"
@@ -17,8 +13,117 @@
               편집하기
             </button>
           </div>
+          <div ref="cardContainer" class="relative">
+            <div class="relative">
+              <div class="sticky">오늘</div>
+              <masonry-wall
+                :items="documentCards"
+                :column-width="258"
+                :gap="16"
+                :scroll-container="cardContainer"
+              >
+                <template #default="{ item }">
+                  <MainDocumentCard
+                    :type="item.type"
+                    :image-url="item.imageUrl"
+                    :url="item.url"
+                    :title="item.title"
+                    :summary="item.summary"
+                    :date="item.date"
+                  />
+                </template>
+              </masonry-wall>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+const cardContainer = ref<HTMLElement | null>(null);
+
+const documentCards = [
+  {
+    type: 'MEMO',
+    imageUrl: '',
+    url: '',
+    title: '메모1',
+    summary: '메모 내용',
+    date: '2021.10.10',
+  },
+  {
+    type: 'IMAGE',
+    imageUrl: 'jkl',
+    url: '',
+    title: '이미지2',
+    summary: '이미지 내용',
+    date: '2021.10.10',
+  },
+  {
+    type: 'WEBPAGE',
+    imageUrl: 'jlk',
+    url: '',
+    title: '링크3',
+    summary: '링크 내용',
+    date: '2021.10.10',
+  },
+  {
+    type: 'FILE',
+    imageUrl: '',
+    url: '',
+    title: '파일4',
+    summary: '파일 내용',
+    date: '2021.10.10',
+  },
+  {
+    type: 'MEMO',
+    imageUrl: '',
+    url: '',
+    title: '메모5',
+    summary: '메모 내용',
+    date: '2021.10.10',
+  },
+  {
+    type: 'IMAGE',
+    imageUrl: 'jkl',
+    url: '',
+    title: '이미지6',
+    summary: '이미지 내용',
+    date: '2021.10.10',
+  },
+  {
+    type: 'WEBPAGE',
+    imageUrl: '',
+    url: '',
+    title: '링크7',
+    summary: '링크 내용',
+    date: '2021.10.10',
+  },
+  {
+    type: 'FILE',
+    imageUrl: 'jkl',
+    url: '',
+    title: '파일8',
+    summary: '파일 내용',
+    date: '2021.10.10',
+  },
+  {
+    type: 'MEMO',
+    imageUrl: '',
+    url: '',
+    title: '메모9',
+    summary: '메모 내용',
+    date: '2021.10.10',
+  },
+  {
+    type: 'IMAGE',
+    imageUrl: 'jkl',
+    url: '',
+    title: '이미지10',
+    summary: '이미지 내용',
+    date: '2021.10.10',
+  },
+];
+</script>
