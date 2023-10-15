fd89db6369714e459bf7599abbc76e424632f886
song-yeonghyun <dudgus907@naver.com>
2023-10-16 00:57:34 +0900

    🎨 Design: 태그검색 창 디자인
    #

--- pages/tag/index.vue
+++ pages/tag/index.vue
@@ -10,6 +10,17 @@
             <p class="font-bold text-[32px]">태그</p>
           </div>
           <div
+            v-if="tags.length > 0"
+            class="flex h-[56px] w-full flex-row mt-9 px-4 items-center rounded-xl bg-white border border-[#e6e8eb]"
+          >
+            <img src="~/assets/icons/icon_search.svg" alt="검색" />
+            <input
+              type="text"
+              class="w-full h-full text-[#646f7c] text-base font-medium outline-none"
+              placeholder="태그를 검색해보세요"
+            />
+          </div>
+          <div
             v-if="tags.length === 0"
             class="flex w-full justify-center mt-32 flex-col items-center mb-40"
           >
