943d462e4eb8d559ffb1333067ab50f295b69e4b
Seungil Kim <ksi990302@gmail.com>
2023-10-24 19:59:05 +0900

    ✨ Feat: 디테일 페이지에서 태그 클릭시 해당 태그 목록으로 이동
    #

--- components/View/TagList.vue
+++ components/View/TagList.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="flex flex-wrap items-center justify-start gap-2">
     <template v-for="tag in tags" :key="tag">
-      <div
-        class="flex items-center justify-start gap-0.5 rounded-md border border-[#e6e8eb] bg-[#fefefe] px-3 py-2"
-      >
-        <p class="text-left text-sm text-[#646f7c]">
-          {{ tag }}
-        </p>
-      </div>
+      <NuxtLink :to="`/tag/${encodeURIComponent(tag)}`">
+        <div
+          class="flex items-center justify-start gap-0.5 rounded-md border border-[#e6e8eb] bg-[#fefefe] px-3 py-2 hover:bg-[#f5f5f5] hover:border-[#dcdcdc]"
+        >
+          <p class="text-left text-sm text-[#646f7c]">
+            {{ tag }}
+          </p>
+        </div>
+      </NuxtLink>
     </template>
   </div>
 </template>
